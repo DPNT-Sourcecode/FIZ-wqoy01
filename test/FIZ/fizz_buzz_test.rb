8c755828072739a5a014bf54310fac712b3c8330
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 11:21:25 +0000

    Mon Apr 08 11:21:25 UTC 2019

--- test/FIZ/fizz_buzz_test.rb
+++ test/FIZ/fizz_buzz_test.rb
@@ -8,13 +8,17 @@ require_solution 'FIZ'
 
 class ClientTest < Minitest::Test
   def test_fizz_buzz
-    assert_equal "fizz", FizzBuzz.new.fizz_buzz(6), 'Write "fizz" if number is multiple of 3'
+    assert_equal 'fizz', FizzBuzz.new.fizz_buzz(6), 'Write "fizz" if number is multiple of 3'
 
-    assert_equal "buzz", FizzBuzz.new.fizz_buzz(25), 'Write "buzz" if number is multiple of 5'
+    assert_equal 'buzz', FizzBuzz.new.fizz_buzz(25), 'Write "buzz" if number is multiple of 5'
 
-    assert_equal "fizz buzz", FizzBuzz.new.fizz_buzz(30), 'Write "fizz buzz" if number is multiple of both 3 and 5'
+    assert_equal 'fizz buzz', FizzBuzz.new.fizz_buzz(30), 'Write "fizz buzz" if number is multiple of both 3 and 5'
 
     assert_equal 11, FizzBuzz.new.fizz_buzz(11), 'Write the number if number is not multiple of either 3 or 5'
+
+    assert_equal 'fizz', FizzBuzz.new.fizz_buzz(13), 'Write "fizz" if number contains a 3'
+
   end
 end
+
 
