449735d3cb7d4238c19bd72c684ae4198d6241ff
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 10:49:09 +0000

    Mon Apr 08 10:49:09 UTC 2019

--- test/FIZ/fizz_buzz_test.rb
+++ test/FIZ/fizz_buzz_test.rb
@@ -9,7 +9,13 @@ require_solution 'FIZ'
 class ClientTest < Minitest::Test
 
   def test_fizz_buzz
-    
+    assert_equal "fizz buzz", FizzBuzz.new.fizz_buzz(15), 'If number divisible by both 3 and 5, return "fizz buzz"'
+
   end
 
 end
+
+# - If the number is a multiple of three then you should write "fizz"
+# - If the number is a multiple of five then you should write "buzz"
+# - If the number is a multiple of both three and five then you should write "fizz buzz"
+# - If the number is not a multiple of five or three then write the number, example 1
