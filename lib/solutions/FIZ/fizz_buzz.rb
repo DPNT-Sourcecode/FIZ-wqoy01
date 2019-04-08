ed2465265c579f5c96e044ce957906928bc9f250
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 12:28:26 +0000

    Mon Apr 08 12:28:26 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -7,18 +7,19 @@ class FizzBuzz
 
     result.push('buzz') if (number % 5).zero? || number.to_s.include?('5')
 
-    result.push('deluxe') if (number > 10)
+    result.push('deluxe') if deluxe?(number)
     result.empty? ? number : result.join(' ')
   end
 
-  def is_deluxe? (number)
-    (number > 10) && all_same_digits? (number)
+  def deluxe?(number)
+    (number > 10) && all_same_digits?(number)
   end
 
-  def all_same_digits? (number)
+  def all_same_digits?(number)
     ary_of_chars = number.to_s.chars
-    ary_of_chars.all? {|digit| digit == ary_of_chars[0]}
+    ary_of_chars.all? { |digit| digit == ary_of_chars[0] }
   end
 end
+
 
 
