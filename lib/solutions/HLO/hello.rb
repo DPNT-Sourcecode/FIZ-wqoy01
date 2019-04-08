2c536a65bec75cd554d31d61f567eff30cb59bbe
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 10:29:31 +0000

    Mon Apr 08 10:29:31 UTC 2019

--- lib/solutions/HLO/hello.rb
+++ lib/solutions/HLO/hello.rb
@@ -1,9 +1,13 @@
 # noinspection RubyUnusedLocalVariable
 class Hello
 
-  def hello(friend_name)
-    'Hello, ' + friend_name + '!'
-  end
+  # def hello(friend_name)
+  #   'Hello, ' + friend_name + '!'
+  # end
 
+  def hello(friend_name)
+    'Hello, World!'
+  end
 end
+
 
