0c48972b8935dd5e0edd5253340a42c2b4d4c7d2
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 10:46:09 +0000

    Mon Apr 08 10:46:09 UTC 2019

---
+++ test/FIZ/fizz_buzz_test.rb
@@ -0,0 +1,15 @@
+# noinspection RubyResolve,RubyResolve
+require_relative '../test_helper'
+require 'logging'
+
+Logging.logger.root.appenders = Logging.appenders.stdout
+
+require_solution 'FIZ'
+
+class ClientTest < Minitest::Test
+
+  def test_fizz_buzz
+    
+  end
+
+end
