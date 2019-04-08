19c3f0a446e6f361432277474ced57cc9d1e1c56
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 14:02:03 +0000

    Mon Apr 08 14:02:03 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -2,11 +2,11 @@
 class FizzBuzz
   def fizz_buzz(number)
     result = []
-    
+
     result.push('fizz') if (number % 3).zero? || number.to_s.include?('3')
-    
+
     result.push('buzz') if (number % 5).zero? || number.to_s.include?('5')
-    
+
     if deluxe?(number)
       chars = number.to_s.chars
       if (number % 3).zero? && chars.include?('3') ||
@@ -14,19 +14,20 @@ class FizzBuzz
         number.odd? ? result.push('fake deluxe') : result.push('deluxe')
       end
     end
-    
+
     result.empty? ? number : result.join(' ')
   end
-  
+
   def deluxe?(number)
     (number > 10) && all_same_digits?(number)
   end
-  
+
   def all_same_digits?(number)
     ary_of_chars = number.to_s.chars
     ary_of_chars.all? { |digit| digit == ary_of_chars[0] }
   end
 end
+
 
 
 
