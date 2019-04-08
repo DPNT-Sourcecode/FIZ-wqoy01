d19b4b5668efa13e41c4098124ca1b1b59e55cda
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 12:19:26 +0000

    Mon Apr 08 12:19:26 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -15,4 +15,3 @@ class FizzBuzz
     
   end
 end
-
--- test/FIZ/fizz_buzz_test.rb
+++ test/FIZ/fizz_buzz_test.rb
@@ -26,4 +26,3 @@ end
 
 # - A number is considered to be "deluxe" if it is greater than 10 and all the digits are identical
 # - A number can be "fizz", "buzz" and "deluxe" at the same time. If this happens then write "fizz buzz deluxe"
-
